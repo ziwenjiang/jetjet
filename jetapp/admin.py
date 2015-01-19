0d2ecdf1bc04d4fca4946d766bae6d3cfbdabc35
akengin <akengin@mit.edu>
2015-01-18 19:13:14 -0500

    7:13
    7:13

--- jetapp/admin.py
+++ jetapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from jetapp.models import Category, Page
+from jetapp.models import UserProfile
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Page)
+admin.site.register(UserProfile)
